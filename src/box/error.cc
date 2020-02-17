580b140289703d4b50916cee6c8ab5bfe661afd2
Nikita Pettik <korablev@tarantool.org>
2020-02-17 21:15:02 +0300

    iproto: support error stacked diagnostic area
    This patch introduces support of stacked errors in IProto protocol and
    in net.box module.
    
    Closes #1148
    
    @TarantoolBot document
    Title: Stacked error diagnostic area
    
    Starting from now errors can be organized into lists. To achieve this
    Lua table representing error object is extended with .prev field and
    e:set_prev(err) method. .prev field returns previous error if any exist.
    e:set_prev(err) method expects err to be error object or nil and sets
    err as previous error of e. For instance:
    ```
    e1 = box.error.new({code = 111, reason = "cause"})
    e2 = box.error.new({code = 111, reason = "cause of cause"})
    
    e1:set_prev(e2)
    assert(e1.prev == e2) -- true
    ```
    Cycles are not allowed for error lists:
    ```
    e2:set_prev(e1)
    - error: 'builtin/error.lua: Cycles are not allowed'
    ```
    Nil is valid input to :set_prev() method:
    ```
    e1:set_prev(nil)
    assert(e1.prev == nil) -- true
    ```
    Note that error can be 'previous' only to the one error at once:
    ```
    e1:set_prev(e2)
    e3:set_prev(e2)
    assert(e1.prev == nil) -- true
    assert(e3.prev == e2) -- true
    ```
    Setting previous error does not erase its own previous members:
    ```
    -- e1 -> e2 -> e3 -> e4
    e1:set_prev(e2)
    e2:set_prev(e3)
    e3:set_prev(e4)
    e2:set_prev(e5)
    -- Now there are two lists: e1->e2->e5 and e3->e4
    assert(e1.prev == e2) -- true
    assert(e2.prev == e5) -- true
    assert(e3.prev == e4) -- true
    ```
    Alternatively:
    ```
    e1:set_prev(e2)
    e2:set_prev(e3)
    e3:set_prev(e4)
    e5:set_prev(e3)
    -- Now there are two lists: e1->e2 and e5->e3->e4
    assert(e1.prev == e2) -- true
    assert(e2.prev == nil) -- true
    assert(e5.prev == e3) -- true
    assert(e3.prev == e4) -- true
    ``
    Stacked diagnostics is also supported by IProto protocol. Now responses
    containing errors always (even if there's only one error to be returned)
    include new IProto key: IPROTO_ERROR_STACK (0x51). So, body corresponding to
    error response now looks like:
    ```
    MAP{IPROTO_ERROR : string, IPROTO_ERROR_STACK : ARRAY[MAP{ERROR_CODE : uint, ERROR_MESSAGE : string}, MAP{...}, ...]}
    ```
    where IPROTO_ERROR is 0x31 key, IPROTO_ERROR_STACK is 0x51, ERROR_CODE
    is 0x01 and ERROR_MESSAGE is 0x02.
    Instances of older versions (without support of stacked errors in
    protocol) simply ignore unknown keys and still rely only on IPROTO_ERROR
    key.

--- src/box/error.cc
+++ src/box/error.cc
@@ -102,6 +102,23 @@ box_error_new(const char *file, unsigned line, uint32_t code,
 	return e;
 }
 
+int
+box_error_add(const char *file, unsigned line, uint32_t code,
+	      const char *fmt, ...)
+{
+	struct error *e = BuildClientError(file, line, ER_UNKNOWN);
+	ClientError *client_error = type_cast(ClientError, e);
+	if (client_error) {
+		client_error->m_errcode = code;
+		va_list ap;
+		va_start(ap, fmt);
+		error_vformat_msg(e, fmt, ap);
+		va_end(ap);
+	}
+	diag_add_error(&fiber()->diag, e);
+	return -1;
+}
+
 /* }}} */
 
 struct rmean *rmean_error = NULL;
