df4e13506dbaa0218ca62164f4467a4f25e76a23
Sergey Bronnikov <sergeyb@tarantool.org>
2020-03-20 06:29:02 +0000

    Fix warnings in help.lua

--- src/lua/help.lua
+++ src/lua/help.lua
@@ -9,20 +9,20 @@ local DOCUMENTATION_VERSION = '2.1'
 
 help = { doc.help }
 tutorial = {}
-tutorial[1] = help[1]
+tutorial[1] = help[1] -- luacheck: ignore 113
 
-local help_function_data = {};
-local help_object_data = {}
+local help_function_data = {}; -- luacheck: ignore 211
+local help_object_data = {} -- luacheck: ignore 211
 
-local function help_call(table, param)
-    return help
+local function help_call(table, param) -- luacheck: ignore 212
+    return help -- luacheck: ignore 113
 end
 
-setmetatable(help, { __call = help_call })
+setmetatable(help, { __call = help_call }) -- luacheck: ignore 113
 
 local screen_id = 1;
 
-local function tutorial_call(table, action)
+local function tutorial_call(table, action) -- luacheck: ignore 212
     if action == 'start' then
         screen_id = 1;
     elseif action == 'next' or action == 'more' then
@@ -46,9 +46,9 @@ local function tutorial_call(table, action)
     return (res:gsub('<version>', DOCUMENTATION_VERSION))
 end
 
-setmetatable(tutorial, { __call = tutorial_call })
+setmetatable(tutorial, { __call = tutorial_call }) -- luacheck: ignore 113
 
 return {
-    help = help;
-    tutorial = tutorial;
+    help = help; -- luacheck: ignore 113
+    tutorial = tutorial; -- luacheck: ignore 113
 }
