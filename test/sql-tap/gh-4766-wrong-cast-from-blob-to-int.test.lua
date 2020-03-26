bed63c7b293d04ba706f102ec4e3af2411f0ec70
Mergen Imeev <imeevma@gmail.com>
2020-03-26 15:30:02 +0300

    sql: fix implicit cast from STRING to INTEGER
    Prior to this patch, STRING, which contains the DOUBLE value,
    could be implicitly cast to INTEGER. This was done by converting
    STRING to DOUBLE and then converting this DOUBLE value to INTEGER.
    This may affect the accuracy of CAST(), so it was forbidden.
    
    Example:
    box.execute("CREATE TABLE t(i INT PRIMARY KEY);")
    
    Before patch:
    box.execute("INSERT INTO t VALUES ('111.1');")
    box.execute("SELECT * FROM t;")
    Result: 111
    
    After patch:
    box.execute("INSERT INTO t VALUES ('1.1');")
    Result: 'Type mismatch: can not convert 1.1 to integer'
    
    box.execute("INSERT INTO t VALUES ('1.0');")
    Result: 'Type mismatch: can not convert 1.0 to integer'
    
    box.execute("INSERT INTO t VALUES ('1.');")
    Result: 'Type mismatch: can not convert 1. to integer'
    
    Part of #4766
    
    @TarantoolBot document
    Title: disallow cast from STRING contais DOUBLE to INTEGER
    
    After the last two patches, explicit and implicit casting from the
    string containing DOUBLE to INTEGER directly will be prohibited.
    The user must use the explicit cast to DOUBLE before the explicit
    or implicit cast to INTEGER. The reason for this is that before
    these patches, such STRINGs were implicitly cast to DOUBLE, and
    then this DOUBLE was implicitly or explicitly cast to INTEGER.
    Because of this, the result of such a cast may differ from what
    the user expects, and the user may not know why.
    
    Example for implicit cast:
    
    box.execute("CREATE TABLE t(i INT PRIMARY KEY);")
    -- Does not work anymore:
    box.execute("INSERT INTO t VALUES ('1.1');")
    -- Right way:
    box.execute("INSERT INTO t VALUES (CAST('1.1' AS DOUBLE));")
    
    Example for explicit cast:
    
    -- Does not work anymore:
    box.execute("SELECT CAST('1.1' AS INTEGER);")
    -- Right way:
    box.execute("SELECT CAST(CAST('1.1' AS DOUBLE) AS INTEGER);")

--- test/sql-tap/gh-4766-wrong-cast-from-blob-to-int.test.lua
+++ test/sql-tap/gh-4766-wrong-cast-from-blob-to-int.test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env tarantool
 test = require("sqltester")
-test:plan(6)
+test:plan(12)
 
 --
 -- Make sure that STRING or BLOB that contains DOUBLE value cannot
@@ -50,6 +50,59 @@ test:do_catchsql_test(
     "gh-4766-6",
     [[
         SELECT CAST(x'322e' AS INTEGER)
+    ]], {
+        1, "Type mismatch: can not convert varbinary to integer"
+    })
+
+--
+-- Make sure that STRING or BLOB that contains DOUBLE value cannot
+-- be implicitly cast to INTEGER.
+--
+test:do_catchsql_test(
+    "gh-4766-7",
+    [[
+        CREATE TABLE t(i INT PRIMARY KEY);
+        INSERT INTO t VALUES ('1.1');
+    ]], {
+        1, "Type mismatch: can not convert 1.1 to integer"
+    })
+
+test:do_catchsql_test(
+    "gh-4766-8",
+    [[
+        INSERT INTO t VALUES (x'312e31');
+    ]], {
+        1, "Type mismatch: can not convert varbinary to integer"
+    })
+
+test:do_catchsql_test(
+    "gh-4766-9",
+    [[
+        INSERT INTO t VALUES ('2.0');
+    ]], {
+        1, "Type mismatch: can not convert 2.0 to integer"
+    })
+
+test:do_catchsql_test(
+    "gh-4766-10",
+    [[
+        INSERT INTO t VALUES (x'322e30');
+    ]], {
+        1, "Type mismatch: can not convert varbinary to integer"
+    })
+
+test:do_catchsql_test(
+    "gh-4766-11",
+    [[
+        INSERT INTO t VALUES ('2.');
+    ]], {
+        1, "Type mismatch: can not convert 2. to integer"
+    })
+
+test:do_catchsql_test(
+    "gh-4766-12",
+    [[
+        INSERT INTO t VALUES (x'322e');
     ]], {
         1, "Type mismatch: can not convert varbinary to integer"
     })
