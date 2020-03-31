d1f8ba41fbbb110f8034f15575cb0246789ea480
Nikita Pettik <korablev@tarantool.org>
2020-03-31 03:14:55 +0300

    vinyl: bump dump_generation in case scheduler doesn't catch up with DDL
    It may turn out that dump_generation does not catch up with current
    generation and no other dump tasks are in progress. This may happen
    dump process is throttled due to errors. In this case generation is
    bumped but dump_generation is not (since dump is not completed). In
    turn, throttling opens a window for DDL operations. For instance, index
    dropping and creation of new one results in mentioned situation:
    
    box.snapshot() -- fails for some reason; next attempt at dumping will be
                   -- taken in one second.
    s:drop() -- drop index to be dumped
    s = box.schema.space.create('test', {engine = 'vinyl'})
    -- create new one (its mem generation is greater than scheduler's one)
    i = s:create_index('pk')
    
    Closes #4821

---
+++ test/vinyl/gh-4821-ddl-during-throttled-dump.test.lua
@@ -0,0 +1,22 @@
+errinj = box.error.injection
+fiber = require('fiber')
+
+-- During this test we verify that if dump process is throttled
+-- due to error, DDL operations in window between dumps won't
+-- break anything.
+--
+s = box.schema.space.create('test', {engine = 'vinyl'})
+_ = s:create_index('pk')
+
+errinj.set('ERRINJ_VY_RUN_WRITE', true)
+s:replace{2}
+box.snapshot()
+errinj.set('ERRINJ_VY_RUN_WRITE', false)
+s:drop()
+
+s = box.schema.space.create('test1', {engine = 'vinyl'})
+i = s:create_index('pk1')
+s:replace{1}
+-- Unthrottle scheduler.
+errinj.set('ERRINJ_VY_SCHED_TIMEOUT', 0)
+s:drop()
